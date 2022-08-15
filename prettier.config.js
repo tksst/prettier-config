4bfcfd7a240e1eabd61b756e71b32ac23dcfb3a3
Takashi Sato <takashi@tks.st>
2022-08-15 23:55:29 +0900

    fix: remove useless trailingComma configuration for YAML

--- prettier.config.js
+++ prettier.config.js
@@ -4,13 +4,13 @@ module.exports = {
     printWidth: 120,
     overrides: [
         {
-            files: "*.{json,json5}",
+            files: "*.{json,json5,yaml,yml}",
             options: {
                 tabWidth: 2,
             },
         },
         {
-            files: "*.{md,yaml,yml}",
+            files: "*.md",
             options: {
                 tabWidth: 2,
                 trailingComma: "none",
